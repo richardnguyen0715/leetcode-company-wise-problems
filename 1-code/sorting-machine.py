6ae8352320f16d6527f466a225028d1eda2d53b5
tuong.nguyen <richardnguyen0715@gmail.com>
2026-02-09 10:35:37 +0700

    chore: remove residual cols

--- 1-code/sorting-machine.py
+++ 1-code/sorting-machine.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 
-df = pd.read_csv("")
+df = pd.read_csv(r'C:\coding\leetcode-company-wise-problems\1-code\leetcode_problems_by_type_and_company.csv')
+df.drop(columns=['Company_List', "Link"], inplace=True)
+df.to_csv(r'C:\coding\leetcode-company-wise-problems\1-code\leetcode_problems_by_type_and_company.csv')
